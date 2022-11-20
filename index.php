334945b352c6be37cd3bc47f37d3dbef75450273
yamadahideto <yahideto428@gmail.com>
2022-11-20 23:59:47 +0900

    create_index・result

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Practice2</title>
+</head>
+<body>
+  <!-- ①フォームを作成しましょう -->
+  <!-- ここにフォームを記述してください -->
+
+</body>
+</html>
+
+
+<?php
+
+
+
+?>
